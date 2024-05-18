1d7ce36581acde2172a8675c31ff49e36dab01d3
Foulques pellabeuf <foulques.pellabeuf@onbrane.com>
2024-05-18 18:18:01 +0200

    feat: programs + test

--- tests/solana-programs-rust.ts
+++ tests/solana-programs-rust.ts
@@ -1,16 +1,109 @@
 import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { SolanaProgramsRust } from "../target/types/solana_programs_rust";
+import { BN, Program, web3 } from "@coral-xyz/anchor";
+import { assert } from "chai";
+import fs from "fs";
 
-describe("solana-programs-rust", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+describe("Test Minter", () => {
+  // Metaplex Constants
+  const METADATA_SEED = "metadata";
+  const TOKEN_METADATA_PROGRAM_ID = new web3.PublicKey("");
+  
+  // Constants from our program
+  const MINT_SEED = "mint";
 
-  const program = anchor.workspace.SolanaProgramsRust as Program<SolanaProgramsRust>;
+  // Data for our tests
+  const payer = pg.wallet.publicKey;
+  const metadata = {
+    name: "TEST V1",
+    symbol: "TEST1",
+    uri: "", // TODO
+    decimals: 9,
+  };
+  const mintAmount = 10;
+  const [mint] = web3.PublicKey.findProgramAddressSync(
+    [Buffer.from(MINT_SEED)],
+    pg.PROGRAM_ID
+  );
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const [metadataAddress] = web3.PublicKey.findProgramAddressSync(
+    [
+      Buffer.from(METADATA_SEED),
+      TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+      mint.toBuffer(),
+    ],
+    TOKEN_METADATA_PROGRAM_ID
+  );
+
+  // Test init token
+  it("initialize", async () => {
+    const info = await pg.connection.getAccountInfo(mint);
+    if (info) {
+      return; // Do not attempt to initialize if already initialized
+    }
+    console.log("  Mint not found. Attempting to initialize.");
+
+    const context = {
+      metadata: metadataAddress,
+      mint,
+      payer,
+      rent: web3.SYSVAR_RENT_PUBKEY,
+      systemProgram: web3.SystemProgram.programId,
+      tokenProgram: anchor.utils.token.TOKEN_PROGRAM_ID,
+      tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID,
+    };
+
+    const txHash = await pg.program.methods
+      .initToken(metadata)
+      .accounts(context)
+      .rpc();
+
+    await pg.connection.confirmTransaction(txHash, "finalized");
+    console.log(`  https://explorer.solana.com/tx/${txHash}?cluster=devnet`);
+    const newInfo = await pg.connection.getAccountInfo(mint);
+    assert(newInfo, "  Mint should be initialized.");
+  });
+
+  // Test mint tokens
+  it("mint tokens", async () => {
+    const destination = await anchor.utils.token.associatedAddress({
+      mint: mint,
+      owner: payer,
+    });
+
+    let initialBalance: number;
+    try {
+      const balance = await pg.connection.getTokenAccountBalance(destination);
+      initialBalance = balance.value.uiAmount;
+    } catch {
+      // Token account not yet initiated has 0 balance
+      initialBalance = 0;
+    }
+
+    const context = {
+      mint,
+      destination,
+      payer,
+      rent: web3.SYSVAR_RENT_PUBKEY,
+      systemProgram: web3.SystemProgram.programId,
+      tokenProgram: anchor.utils.token.TOKEN_PROGRAM_ID,
+      associatedTokenProgram: anchor.utils.token.ASSOCIATED_PROGRAM_ID,
+    };
+
+    const txHash = await pg.program.methods
+      .mintTokens(new BN(mintAmount * 10 ** metadata.decimals))
+      .accounts(context)
+      .rpc();
+    await pg.connection.confirmTransaction(txHash);
+    console.log(`  https://explorer.solana.com/tx/${txHash}?cluster=devnet`);
+
+    const postBalance = (
+      await pg.connection.getTokenAccountBalance(destination)
+    ).value.uiAmount;
+    assert.equal(
+      initialBalance + mintAmount,
+      postBalance,
+      "Post balance should equal initial plus mint amount"
+    );
   });
 });
+
